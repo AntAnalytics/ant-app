2423f378d54302d10e46974a62448f0664b3d7fa
AntAnalytics <theantanalytics@gmail.com>
2022-05-08 16:46:04 +0530

    google login added

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from 'lib/prisma';
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   // https://next-auth.js.org/configuration/providers
   providers: [
     GoogleProvider({
@@ -9,33 +12,18 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
-
-  // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
-  // https://next-auth.js.org/configuration/databases
-  //
-  // Notes:
-  // * You must install an appropriate node_module for your database
-  // * The Email provider requires a database (OAuth providers do not)
-  // database: process.env.DATABASE_URL,
-
-  // The secret should be set to a reasonably long random string.
-  // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
-  // a separate secret is defined explicitly for encrypting the JWT.
   secret: process.env.SECRET,
 
   session: {
-    // Use JSON Web Tokens for session instead of database sessions.
-    // This option can be used with or without a database for users/accounts.
-    // Note: `strategy` should be set to 'jwt' if no database is used.
     strategy: 'jwt',
 
     // Seconds - How long until an idle session expires and is no longer valid.
-    // maxAge: 30 * 24 * 60 * 60, // 30 days
+    maxAge: 30 * 24 * 60 * 60, // 30 days
 
     // Seconds - Throttle how frequently to write to database to extend a session.
     // Use it to limit write operations. Set to 0 to always update the database.
     // Note: This option is ignored if using JSON Web Tokens
-    // updateAge: 24 * 60 * 60, // 24 hours
+    updateAge: 24 * 60 * 60, // 24 hours
   },
 
   // JSON Web tokens are only used for sessions if the `strategy: 'jwt'` session
@@ -44,19 +32,8 @@ export default NextAuth({
   jwt: {
     // A secret to use for key generation (you should set this explicitly)
     secret: process.env.SECRET,
-    // Set to true to use encryption (default: false)
-    // encryption: true,
-    // You can define your own encode/decode functions for signing and encryption
-    // if you want to override the default behaviour.
-    // encode: async ({ secret, token, maxAge }) => {},
-    // decode: async ({ secret, token, maxAge }) => {},
   },
 
-  // You can define custom pages to override the built-in ones. These will be regular Next.js pages
-  // so ensure that they are placed outside of the '/api' folder, e.g. signIn: '/auth/mycustom-signin'
-  // The routes shown here are the default URLs that will be used when a custom
-  // pages is not specified for that route.
-  // https://next-auth.js.org/configuration/pages
   pages: {
     // signIn: '/auth/signin',  // Displays signin buttons
     // signOut: '/auth/signout', // Displays form with sign out button
@@ -65,8 +42,6 @@ export default NextAuth({
     // newUser: null // If set, new users will be directed here on first sign in
   },
 
-  // Callbacks are asynchronous functions you can use to control what happens
-  // when an action is performed.
   // https://next-auth.js.org/configuration/callbacks
   callbacks: {
     // async signIn({ user, account, profile, email, credentials }) { return true },
@@ -75,10 +50,6 @@ export default NextAuth({
     // async jwt({ token, user, account, profile, isNewUser }) { return token }
   },
 
-  // Events are useful for logging
   // https://next-auth.js.org/configuration/events
   events: {},
-
-  // Enable debug messages in the console if you are having problems
-  debug: false,
 });
---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,25 @@
+import { NextPage } from 'next';
+import PageLayout from 'layouts/PageLayout';
+import { useSession, signIn } from 'next-auth/react';
+
+interface DashBoardPageProps {}
+
+const DashBoardPage: NextPage<DashBoardPageProps> = () => {
+  const { data: session, status } = useSession();
+  //   console.log({ session });
+  return (
+    <PageLayout>
+      {status === 'authenticated' ? (
+        <div>
+          <p>Signed in as {session.user?.email}</p>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => signIn('google')}>Sign in</button>
+        </div>
+      )}
+    </PageLayout>
+  );
+};
+
+export default DashBoardPage;
