a673ab396d066174c4e6497679085b0b953d94f8
AntAnalytics <theantanalytics@gmail.com>
2022-05-14 15:27:33 +0530

    tabs added

--- layouts/dashboard.tsx
+++ layouts/dashboard.tsx
@@ -22,7 +22,12 @@ const navigation = [
   // { name: 'Team', href: '#', icon: UsersIcon, current: false },
   // { name: 'Projects', href: '#', icon: FolderIcon, current: false },
   // { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
+  {
+    name: 'Smart Documents',
+    href: '/smart-documentation',
+    icon: InboxIcon,
+    current: false,
+  },
   { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
 ];
 const userNavigation = [
