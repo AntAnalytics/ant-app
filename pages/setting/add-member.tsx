df6e82e81923efb978958b5a9731f5a619349baa
AntAnalytics <theantanalytics@gmail.com>
2022-06-27 23:59:20 +0530

    edit user updated

--- pages/setting/add-member.tsx
+++ pages/setting/add-member.tsx
@@ -375,7 +375,7 @@ function AddTeamMemberPage({}: InferGetServerSidePropsType<
               type='submit'
               className='ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
             >
-              Add user
+              {id ? 'Update user' : 'Add user'}
             </button>
           </div>
         </div>
--- pages/setting/team-members.tsx
+++ pages/setting/team-members.tsx
@@ -182,15 +182,16 @@ function SettingTeamMemberPage({}: InferGetServerSidePropsType<
                                       {user.role}
                                     </td>
                                     <td className='relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6'>
-                                      <a
-                                        href='#'
-                                        className='text-indigo-600 hover:text-indigo-900'
+                                      <Link
+                                        href={`/setting/add-member?id=${user.id}`}
                                       >
-                                        Edit
-                                        <span className='sr-only'>
-                                          , {user.name}
-                                        </span>
-                                      </a>
+                                        <a className='text-indigo-600 hover:text-indigo-900'>
+                                          Edit
+                                          <span className='sr-only'>
+                                            , {user.name}
+                                          </span>
+                                        </a>
+                                      </Link>
                                     </td>
                                   </tr>
                                 ))}
