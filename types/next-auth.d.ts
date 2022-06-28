8680df50c99ead4af1619960531c71b493e7a6dc
AntAnalytics <theantanalytics@gmail.com>
2022-06-28 14:19:35 +0530

    type fixed

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -6,17 +6,6 @@ declare module 'next-auth' {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user: {
-      id: string;
-      site: string;
-      employeeId: string;
-      name: string;
-      email: string;
-      department: string;
-      image: string;
-      designation: string;
-      role: Role;
-      mobile: string;
-    } & DefaultSession['user'];
+    user: User & DefaultSession['user'];
   }
 }
