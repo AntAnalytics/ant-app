d666208d69cbf368ab82cfff0174eebbdc4387ef
AntAnalytics <theantanalytics@gmail.com>
2022-08-30 19:29:40 +0530

    bg-primary

--- components/headers/ModernHeader.tsx
+++ components/headers/ModernHeader.tsx
@@ -23,7 +23,7 @@ export default function ModernHeader() {
   const { data: session, status } = useSession();
 
   return (
-    <Disclosure as='nav' className='bg-white shadow'>
+    <Disclosure as='nav' className='bg-primary shadow'>
       {({ open }) => (
         <>
           <div className='mx-auto px-2 sm:px-6 lg:px-8'>
