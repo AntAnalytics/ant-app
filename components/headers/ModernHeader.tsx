da888afaa895be6c403dad7e12931e9b98895be0
AntAnalytics <theantanalytics@gmail.com>
2022-08-30 19:01:06 +0530

    ui updated

--- components/headers/ModernHeader.tsx
+++ components/headers/ModernHeader.tsx
@@ -23,7 +23,7 @@ export default function ModernHeader() {
   const { data: session, status } = useSession();
 
   return (
-    <Disclosure as='nav' className='bg-white shadow'>
+    <Disclosure as='nav' className='bg-primary shadow'>
       {({ open }) => (
         <>
           <div className='mx-auto px-2 sm:px-6 lg:px-8'>
@@ -43,7 +43,7 @@ export default function ModernHeader() {
                 {/* Profile dropdown */}
                 <Menu as='div' className='relative ml-3'>
                   <div>
-                    <Menu.Button className='flex items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
+                    <Menu.Button className='flex items-center rounded-full  text-sm '>
                       <span className='sr-only'>Open user menu</span>
                       <Image
                         className='h-8 w-8 rounded-full'
@@ -64,7 +64,7 @@ export default function ModernHeader() {
                     leaveFrom='transform opacity-100 scale-100'
                     leaveTo='transform opacity-0 scale-95'
                   >
-                    <Menu.Items className='absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                    <Menu.Items className='absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg  focus:outline-none'>
                       <Menu.Item key={'name'}>
                         {({ active }) => (
                           <>
